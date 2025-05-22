e12a22e0c996579b6314087486703a1bbaf6efeb
Enock Korir <enockorir123@gmail.com>
2025-05-22 18:07:20 +0300

    counting

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        # Split on sentence-ending punctuation marks: ., !, ?
+        # Use regex to split on one or more of these punctuation marks
+        parts = re.split(r'[.!?]+', self._value)
+        # Filter out empty strings after split
+        sentences = [part.strip() for part in parts if part.strip()]
+        return len(sentences)
